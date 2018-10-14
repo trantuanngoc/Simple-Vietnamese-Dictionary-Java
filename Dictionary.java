bddf3db1d3c60285e485fe7c4df7b223ace98744
trantuanngoc <38138845+trantuanngoc@users.noreply.github.com>
2018-10-14 23:52:09 +0700

    Add files via upload

---
+++ Dictionary.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Dictionary  {
+
+       private static int n;
+        public Word[] words;
+        public Dictionary (){};
+        public void Dictionary(int n){
+            this.n=n;
+             //this.words =new Word[n];
+        }
+      
+       public int getN() {
+        return n;
+        }
+       public void setN(int n){
+           this.n=n;
+       }
+    public void Dictionary(Word[] words) {
+		
+		this.words = words;
+	}
+}
