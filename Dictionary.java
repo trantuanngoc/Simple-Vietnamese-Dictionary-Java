34a884421fe9d82d885809a055e7f648b1b1fe07
trantuanngoc <38138845+trantuanngoc@users.noreply.github.com>
2018-10-22 00:01:26 +0700

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dictionary1;
+package miniproject;
 
 /**
  *
@@ -17,7 +17,7 @@ public class Dictionary  {
         
         public void Dictionary(int n){
             this.n=n;
-             //this.words =new Word[n];
+             
         }
       
        public int getN() {
